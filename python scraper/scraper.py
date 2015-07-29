828414720d6ba2355fa7687fa6a4970355dc3225
William Thammavong <thammavo@eng.utah.edu>
2015-07-29 16:26:53 -0600

    python web scraper
    Added a python web scraper version

---
+++ python scraper/scraper.py
@@ -0,0 +1,21 @@
+#!/bin/env python
+
+# Author: William Thammavong
+# Date: 7/29/1015
+# python web scraper
+
+import scrapy
+
+class BlogSpider(scrapy.Spider):
+    name = 'blogspider'
+    start_urls = ['http://blog.scrapinghub.com']
+
+    def parse(self, response):
+        for url in response.css('ul li a::attr("href")').re(r'.*/\d\d\d\d/\d\d/$'):
+            yield scrapy.Request(response.urljoin(url), self.parse_titles)
+
+    def parse_titles(self, response):
+        for post_title in response.css('div.entries > ul > li a::text').extract():
+            yield {'title': post_title}
+EOF
+ scrapy runspider myspider.py
